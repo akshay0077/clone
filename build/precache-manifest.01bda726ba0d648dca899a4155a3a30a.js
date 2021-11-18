0a1898918fb4c386a8547cd16b89f6e662a6ac29
akshay0077 <kherakshay007@gmail.com>
2021-11-18 18:53:05 +0530

    first

---
+++ build/precache-manifest.01bda726ba0d648dca899a4155a3a30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdc07c60f1e4cc43c26264793465bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "980a9e6b73577eb70793",
+    "url": "/static/css/main.ed3d4918.chunk.css"
+  },
+  {
+    "revision": "87a9b186c7c9bbb8d8ef",
+    "url": "/static/js/2.52df69ca.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.52df69ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980a9e6b73577eb70793",
+    "url": "/static/js/main.545838ce.chunk.js"
+  },
+  {
+    "revision": "42236b3c5a0e9054fb96",
+    "url": "/static/js/runtime-main.0c82e416.js"
+  }
+]);
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY="AIzaSyCYAcUWMUiSRYBMxPlXNgbVUIAZ48mTlfs"
+
+export default API_KEY;
